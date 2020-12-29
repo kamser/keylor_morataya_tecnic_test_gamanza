f775af0cd42f6c8ad2f418e19e7013a92e2994ff
kamser <keylor.morataya@gmail.com>
2020-12-28 18:43:22 -0600

    Modified the App index for use routes between page

--- breaking-bad-app/src/App.tsx
+++ breaking-bad-app/src/App.tsx
@@ -2,24 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {Route, BrowserRouter as Router, Switch} from 'react-router-dom'
+
+import SearchAppBar from './components/generalComponents/navigationBar';
+import EpisodePage from './components/episodePage/index';
+import HomePage from './components/homePage/index';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="App">
+        <SearchAppBar/>
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route exact path='/episodes' component={EpisodePage}/>
+          <Route exact path='/home' component={HomePage}/>
+        </Switch>
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header> */}
     </div>
+    </Router>
   );
 }
 
