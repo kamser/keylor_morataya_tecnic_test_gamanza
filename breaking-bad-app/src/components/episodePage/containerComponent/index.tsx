9a39e91d1887a3c56f2b55b60e959f69b51b5a01
kamser <keylor.morataya@gmail.com>
2020-12-28 20:19:35 -0600

    last update for episode page in the container component

--- breaking-bad-app/src/components/episodePage/containerComponent/index.tsx
+++ breaking-bad-app/src/components/episodePage/containerComponent/index.tsx
@@ -1,9 +1,51 @@
 import React from 'react';
+import EpisodeCard from '../episodeCardComponent/index';
+import Grid from '@material-ui/core/Grid';
 
 
 const Container = () => {
+
+    const seasonEpisodesData = [
+        {
+            title: 'title',
+            season: 'season',
+            episode: 'episode',
+            airDate: 'airDate',
+        },
+        {
+            title: 'title2',
+            season: 'season2',
+            episode: 'episode2',
+            airDate: 'airDate2',
+        },
+        {
+            title: 'title3',
+            season: 'season3',
+            episode: 'episode3',
+            airDate: 'airDate3',
+        },
+        {
+            title: 'title4',
+            season: 'season4',
+            episode: 'episode4',
+            airDate: 'airDate4',
+        },
+        
+    ];
     return(
         <>
+            <Grid>
+                {
+                    seasonEpisodesData.map((episodeData, index) => {
+                        return(
+                            <Grid key={index}>
+                                <EpisodeCard title={episodeData.title} season={episodeData.season} episode={episodeData.episode} airDate={episodeData.airDate}/>
+                            </Grid>
+                        )
+                    })
+                }
+                
+            </Grid>
         </>
     );
 }
--- breaking-bad-app/src/components/episodePage/episodeCardComponent/index.tsx
+++ breaking-bad-app/src/components/episodePage/episodeCardComponent/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
+interface propsEpisodeCard{
+    title: string;
+    season: string;
+    episode: string;
+    airDate: string;
+}
 
-const EpisodeCard = () => {
+const EpisodeCard = (props: propsEpisodeCard) => {
+    const {title, season, episode, airDate} = props;
     return(
         <>
+            <Grid>
+                <Grid>
+                    <Paper>
+                        <Typography variant='h6'>{title}</Typography>
+                        <Typography variant='h6'>{season}</Typography>
+                        <Typography variant='h6'>{episode}</Typography>
+                        <Typography variant='h6'>{airDate}</Typography>
+                    </Paper>
+                </Grid>
+            </Grid>
         </>
     );
 }
